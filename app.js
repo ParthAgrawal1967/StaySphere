2a34c960277efcf19ff9da5387b29112e41630c2
Parth Agrawal <parthagrawal1967@gmail.com>
2025-08-10 14:51:17 +0530

    added authentication

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const ejsMate=require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
 const session=require("express-session");
 const flash=require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
 
-const listings=require("./routes/listing.js");
-const reviews=require("./routes/review.js");
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -37,11 +41,27 @@ app.get("/",(req,res)=>{
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
     next();
 })
+
+// app.get("/demouser", async(req,res)=>{
+//   let fakeUser=new User({
+//     email:"fakestudemt@gmail.com",
+//     username: "Mansi",
+//   });
+//   let newUser=await User.register(fakeUser,"Unstoppable");
+//   res.send(newUser);
+// });
 
 //Port connection
 const port=3000;
@@ -64,8 +84,9 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 app.all("/{*any}",(req,res,next)=>{
     next(new ExpressError(404,"Page not found"));
